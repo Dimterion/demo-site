d46057686df9b40864e602a24e00f7665b90aeb8
Dmitrii <dimterion59@gmail.com>
2024-09-06 12:16:15 +0200

    add header component, adjust favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <>
-      <h1 className="3xl font-bold underline">App</h1>
       <div className="overflow-hidden pt-[4.75rem] lg:pt-[5.25rem]"></div>
+      <Header />
       <ButtonGradient />
     </>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import headerLogo from "../assets/header_logo.png";
+
+const Header = () => {
+  return (
+    <div className="fixed top-0 z-50 border-b border-n-6 bg-n-8/90 backdrop-blur-sm lg:bg-n-8/90 lg:backdrop-blur-sm">
+      <div className="flex items-center px-5 max-lg:py-4 lg:px-7.5 xl:px-10">
+        <a className="block w-[12rem] xl:mr-8" href="#hero">
+          <img src={headerLogo} width={190} height={40} alt="Header logo" />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
