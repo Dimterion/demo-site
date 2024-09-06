b390fe4e1cf91417fa082601959a5a787f077c62
Dmitrii <dimterion59@gmail.com>
2024-09-06 11:57:52 +0200

    add btn component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
-function App() {
-  return <main>App</main>;
-}
+import ButtonGradient from "./assets/svg/ButtonGradient";
+
+const App = () => {
+  return (
+    <>
+      <h1 className="3xl font-bold underline">App</h1>
+      <div className="overflow-hidden pt-[4.75rem] lg:pt-[5.25rem]"></div>
+      <ButtonGradient />
+    </>
+  );
+};
 
 export default App;
