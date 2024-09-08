3385549e6d27a2221d00414ba163d0d2c37cbf4f
Dmitrii <dimterion59@gmail.com>
2024-09-08 10:36:49 +0200

    adjust header elements

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -25,14 +25,16 @@ const Header = () => {
 
   return (
     <div
-      className={`fixed left-0 top-0 z-50 w-full border-b border-n-6 bg-n-8/90 backdrop-blur-sm lg:bg-n-8/90 lg:backdrop-blur-sm ${openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"}`}
+      className={`fixed left-0 top-0 z-50 w-full border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"}`}
     >
       <div className="flex items-center px-5 max-lg:py-4 lg:px-7.5 xl:px-10">
         <a className="block w-[12rem] xl:mr-8" href="#hero">
           <img src={headerLogo} width={50} height={20} alt="Header logo" />
         </a>
         <nav
-          className={`${openNavigation ? "flex" : "hidden"} fixed bottom-0 left-0 top-[5rem] bg-n-8 ring-0 lg:static lg:mx-auto lg:flex lg:bg-transparent`}
+          className={`${
+            openNavigation ? "flex" : "hidden"
+          } fixed bottom-0 left-0 right-0 top-[5rem] bg-n-8 lg:static lg:mx-auto lg:flex lg:bg-transparent`}
         >
           <div className="relative z-2 m-auto flex flex-col items-center justify-center lg:flex-row">
             {navigation.map((item) => (
