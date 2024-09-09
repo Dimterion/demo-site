bceaa5c18a1ce05bb5413abbe4668a2d8e1e3a3a
Dmitrii <dimterion59@gmail.com>
2024-09-09 09:58:01 +0200

    add section component

---
+++ src/assets/svg/SectionSvg.jsx
@@ -0,0 +1,27 @@
+import PropTypes from "prop-types";
+
+import PlusSvg from "./PlusSvg";
+
+const SectionSvg = ({ crossesOffset }) => {
+  return (
+    <>
+      <PlusSvg
+        className={`absolute -top-[0.3125rem] left-[1.5625rem] hidden ${
+          crossesOffset && crossesOffset
+        } pointer-events-none lg:block xl:left-[2.1875rem]`}
+      />
+
+      <PlusSvg
+        className={`absolute -top-[0.3125rem] right-[1.5625rem] hidden ${
+          crossesOffset && crossesOffset
+        } pointer-events-none lg:block xl:right-[2.1875rem]`}
+      />
+    </>
+  );
+};
+
+SectionSvg.propTypes = {
+  crossesOffset: PropTypes.string,
+};
+
+export default SectionSvg;
