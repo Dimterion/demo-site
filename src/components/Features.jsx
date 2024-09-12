08f2d84f10d0c6baff92a7ab0acb7e04333e1048
Dmitrii <dimterion59@gmail.com>
2024-09-12 10:45:58 +0200

    adjust features component elements

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -13,14 +13,23 @@ const Features = () => {
         <div className="mb-10 flex flex-wrap gap-10">
           {features.map((item) => (
             <div
-              className="relative block bg-[length:100%_100%] bg-no-repeat p-0.5 md:max-w-[24rem]"
+              className="relative block bg-cover bg-no-repeat p-0.5 md:max-w-[24rem]"
               style={{
                 backgroundImage: `url(${item.backgroundUrl})`,
               }}
               key={item.id}
             >
-              <div>
-                <h5>{item.title}</h5>
+              <div className="mih-h-[22rem] pointer-events-none relative z-2 flex flex-col p-[2.4rem]">
+                <h5 className="h2 mb-5">{item.title}</h5>
+                <p className="body-2 mb-6 text-n-3">{item.text}</p>
+                <div className="mt-auto flex items-center">
+                  <img
+                    src={item.iconUrl}
+                    width={48}
+                    height={48}
+                    alt={item.title}
+                  />
+                </div>
               </div>
             </div>
           ))}
