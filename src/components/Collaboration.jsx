4ff72944053bd59f15b93c03551e0f9aa7d9dbe7
Dmitrii <dimterion59@gmail.com>
2024-09-14 11:54:06 +0200

    adjust collaboration component elements & add logo

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -1,6 +1,8 @@
 import { FaCheckCircle } from "react-icons/fa";
-import { collabContent } from "../constants";
+import { collabContent, collabText } from "../constants";
+import headerLogo from "../assets/headerLogo.png";
 import Section from "./Section";
+import Button from "./Button";
 
 const Collaboration = () => {
   return (
@@ -21,6 +23,26 @@ const Collaboration = () => {
               </li>
             ))}
           </ul>
+          <Button>Try it now</Button>
+        </div>
+        <div className="mt-4 lg:ml-auto xl:w-[38rem]">
+          <p className="body-2 mb-4 text-n-4 md:mb-16 lg:mx-auto lg:mb-32 lg:w-[22rem]">
+            {collabText}
+          </p>
+          <div className="relative left-1/2 flex aspect-square w-[22rem] -translate-x-1/2 scale-75 rounded-full border border-n-6 md:scale-100">
+            <div className="m-auto flex aspect-square w-60 rounded-full border border-n-6">
+              <div className="m-auto aspect-square w-[6rem] rounded-full bg-conic-gradient p-[0.2rem]">
+                <div className="flex h-full w-full items-center justify-center rounded-full bg-n-8">
+                  <img
+                    src={headerLogo}
+                    width={48}
+                    height={48}
+                    alt="Header logo"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </Section>
