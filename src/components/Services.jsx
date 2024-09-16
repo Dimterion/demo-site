a1767c877791561dd5899d841dc0d0673b5f7cc5
Dmitrii <dimterion59@gmail.com>
2024-09-16 10:54:27 +0200

    add content & adjust services component style

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,5 +1,6 @@
 import Section from "./Section";
 import Heading from "./Heading";
+import Generating from "./Generating";
 import backgroundImage from "../assets/backgroundImg.jpg";
 import { dsServices } from "../constants";
 import { FaCheckCircle } from "react-icons/fa";
@@ -39,6 +40,26 @@ const Services = () => {
                   </li>
                 ))}
               </ul>
+            </div>
+            <Generating className="lg-right-auto absolute bottom-4 left-4 right-4 border border-n-1/10 lg:bottom-8 lg:left-1/2 lg:-translate-x-1/2" />
+          </div>
+          <div className="relative z-1 grid gap-5 lg:grid-cols-2">
+            <div className="relative min-h-[39rem] overflow-hidden rounded-3xl border border-n-1/10">
+              <div className="absolute inset-0">
+                <img
+                  className="h-full w-full object-cover"
+                  width={630}
+                  height={750}
+                  alt="Background image"
+                  src={backgroundImage}
+                />
+              </div>
+              <div className="absolute inset-0 flex flex-col justify-end bg-gradient-to-b from-n-8/0 to-n-8/90 p-8 lg:p-15">
+                <h4 className="h4 mb-4">Lorem text</h4>
+                <p className="body-2 mb-[3rem] text-n-3">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.{" "}
+                </p>
+              </div>
             </div>
           </div>
         </div>
