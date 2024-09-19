917e15c87ca8d32fe3a358c894d4af48730a23ea
Dmitrii <dimterion59@gmail.com>
2024-09-19 10:11:53 +0200

    adjust tagline & heading, add brackets component

--- src/components/TagLine.jsx
+++ src/components/TagLine.jsx
@@ -1,5 +1,19 @@
-const TagLine = () => {
-  return <div>TagLine</div>;
+import PropTypes from "prop-types";
+import brackets from "../assets/svg/Brackets";
+
+const TagLine = ({ className, children }) => {
+  return (
+    <div className={`tagline flex items-center ${className || ""}`}>
+      {brackets("left")}
+      <div className="mx-3 text-n-3">{children}</div>
+      {brackets("right")}
+    </div>
+  );
+};
+
+TagLine.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.string,
 };
 
 export default TagLine;
