acf0018b5e8bf5e9fe3736c7cd7fae2344ea1277
Dmitrii <dimterion59@gmail.com>
2024-09-21 10:12:04 +0200

    add roadmap component

---
+++ src/components/Roadmap.jsx
@@ -0,0 +1,14 @@
+import Section from "./Section";
+import Heading from "./Heading";
+
+const Roadmap = () => {
+  return (
+    <Section className="overflow-hidden" id="roadmap">
+      <div className="container md:pb-10">
+        <Heading tag="Get started" title="Lorem text title" />
+      </div>
+    </Section>
+  );
+};
+
+export default Roadmap;
