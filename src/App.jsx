1960bb1ad209039bdac8f5dfe2a892b46b5a3178
Dmitrii <dimterion59@gmail.com>
2024-09-23 10:57:09 +0200

    adjust favicon & app component

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,16 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Features from "./components/Features";
 import Collaboration from "./components/Collaboration";
-import ButtonGradient from "./assets/svg/ButtonGradient";
 import Services from "./components/Services";
 import Pricing from "./components/Pricing";
 import Roadmap from "./components/Roadmap";
 import Footer from "./components/Footer";
+import ButtonGradient from "./assets/svg/ButtonGradient";
 
 const App = () => {
   return (
     <>
-      <div className="overflow-hidden pt-[4.75rem] lg:pt-[5.25rem]">
+      <main className="overflow-hidden pt-24">
         <Header />
         <Hero />
         <Features />
@@ -20,7 +20,7 @@ const App = () => {
         <Pricing />
         <Roadmap />
         <Footer />
-      </div>
+      </main>
       <ButtonGradient />
     </>
   );
