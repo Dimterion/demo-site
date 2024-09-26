862bc665b726e3ba44baca3bca2bda1a8f6f4fe9
Dmitrii <dimterion59@gmail.com>
2024-09-26 12:30:50 +0200

    adjust Footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,6 +16,7 @@ const Footer = () => {
               key={item.id}
               href={item.url}
               target="_blank"
+              rel="noopener noreferrer"
               className="flex h-10 w-10 items-center justify-center rounded-full bg-n-7 transition-colors hover:bg-n-6"
             >
               <IoShareSocial size={26} />
