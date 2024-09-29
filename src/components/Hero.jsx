0273bfa8f42c17088337a8db673bfc4c392e7be7
Dmitrii <dimterion59@gmail.com>
2024-09-29 10:40:09 +0200

    adjust hero component elements

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,8 +23,8 @@ const Hero = () => {
       customPaddings
       id="hero"
     >
-      <div className="container relative" ref={parallaxRef}>
-        <div className="relative z-1 mx-auto mb-[4rem] max-w-[62rem] text-center md:mb-20 lg:mb-[6rem]">
+      <section className="container relative" ref={parallaxRef}>
+        <article className="relative z-1 mx-auto mb-[4rem] max-w-[62rem] text-center md:mb-20 lg:mb-[6rem]">
           <h1 className="h1 mb-6">
             Explore the Possibilities{" "}
             <span className="relative inline-block">
@@ -45,8 +45,8 @@ const Hero = () => {
           <Button href="/pricing" sunglow>
             Get started
           </Button>
-        </div>
-        <div className="lx:mb-24 relative mx-auto max-w-[23rem] md:max-w-5xl">
+        </article>
+        <article className="lx:mb-24 relative mx-auto max-w-[23rem] md:max-w-5xl">
           <div className="relative z-1 rounded-2xl bg-conic-gradient p-0.5">
             <div className="relative rounded-[1rem] bg-n-8">
               <div className="h-[1.4rem] rounded-t-[0.9rem] bg-n-10" />
@@ -78,19 +78,19 @@ const Hero = () => {
             </div>
             <Gradient />
           </div>
-          <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
+          <aside className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
             <img
               src={heroBackground}
               className="w-full"
               width={1440}
               height={1800}
-              alt="hero"
+              alt="Hero section background"
             />
-          </div>
+          </aside>
           <BackgroundCircles />
-        </div>
+        </article>
         <Logos className="relative z-10 mt-20 hidden lg:block" />
-      </div>
+      </section>
       <BottomLine />
     </Section>
   );
