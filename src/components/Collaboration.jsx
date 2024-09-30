79d6b0bfd7c1cad41e3c7eb4e3009d1f4ff6594d
Dmitrii <dimterion59@gmail.com>
2024-09-30 10:34:09 +0200

    adjust collaboration component html elements

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -10,7 +10,7 @@ const Collaboration = () => {
   return (
     <Section crosses>
       <div className="container lg:flex">
-        <div className="max-w-[25rem]">
+        <article className="max-w-[25rem]">
           <h2 className="h2 mb-4 md:mb-8">App for collaboration</h2>
           <ul className="mb-10 max-w-[22rem] md:mb-14">
             {collabContent.map((item) => (
@@ -26,13 +26,13 @@ const Collaboration = () => {
             ))}
           </ul>
           <Button>Try it now</Button>
-        </div>
-        <div className="mt-4 lg:ml-auto xl:w-[38rem]">
+        </article>
+        <article className="mt-4 lg:ml-auto xl:w-[38rem]">
           <p className="body-2 mb-8 text-n-4 md:mb-16 lg:mx-auto lg:mb-32 lg:w-[22rem]">
             {collabText}
           </p>
-          <div className="relative left-1/2 flex aspect-square w-[22rem] -translate-x-1/2 scale-75 rounded-full border border-n-6 md:scale-100">
-            <div className="m-auto flex aspect-square w-60 rounded-full border border-n-6">
+          <section className="relative left-1/2 flex aspect-square w-[22rem] -translate-x-1/2 scale-75 rounded-full border border-n-6 md:scale-100">
+            <article className="m-auto flex aspect-square w-60 rounded-full border border-n-6">
               <div className="m-auto aspect-square w-[6rem] rounded-full bg-conic-gradient p-[0.2rem]">
                 <div className="flex h-full w-full items-center justify-center rounded-full bg-n-8">
                   <img
@@ -43,7 +43,7 @@ const Collaboration = () => {
                   />
                 </div>
               </div>
-            </div>
+            </article>
             <ul>
               {collabApps.map((app, index) => (
                 <li
@@ -66,8 +66,8 @@ const Collaboration = () => {
             </ul>
             <LeftCurve />
             <RightCurve />
-          </div>
-        </div>
+          </section>
+        </article>
       </div>
     </Section>
   );
